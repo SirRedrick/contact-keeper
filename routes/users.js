905bd1ff9604beccd5779a93d9d47849f7a686d6
Mykyta Batrak <fighter662@gmail.com>
2021-01-06 15:37:24 +0200

    Added a User model with validation

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+const User = require('../models/User');
+
 const router = express.Router();
 
 /*
@@ -6,8 +9,21 @@ const router = express.Router();
  * @decs      Refister a user
  * @access    Public
  */
-router.post('/', (req, res) => {
-	res.send('Register a user');
-});
+router.post(
+	'/',
+	[
+		check('name', 'Please add a name').not().isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		res.send('Passed');
+	}
+);
 
 module.exports = router;
