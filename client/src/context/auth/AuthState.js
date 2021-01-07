de06dfaf2c990ee10583d6df56f6e349f055b500
Mykyta Batrak <fighter662@gmail.com>
2021-01-07 21:38:52 +0200

    Added context for authentication

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,51 @@
+import React, { useReducer } from 'react';
+import AuthContex from './authContext';
+import authReducer from './authReducer';
+import {
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	LOGOUT,
+	CLEAR_ERRORS,
+} from '../types';
+
+const AuthState = (props) => {
+	const initialState = {
+		token: localStorage.getItem('token'),
+		isAuthenticated: null,
+		loading: true,
+		user: null,
+		error: null,
+	};
+
+	const [state, dispatch] = useReducer(authReducer, initialState);
+
+	// Load User
+
+	// Register User
+
+	// Login User
+
+	// Logout
+
+	// Clear Errors
+
+	return (
+		<AuthContex.Provider
+			value={{
+				token: state.token,
+				isAthenticated: state.isAthenticated,
+				loading: state.loading,
+				user: state.user,
+				error: state.error,
+			}}
+		>
+			{props.children}
+		</AuthContex.Provider>
+	);
+};
+
+export default AuthState;
